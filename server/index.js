175486b9ea29da346cb3f1c967f0ca95dc8b709a
faris <faris38kanbur@gmail.com>
2021-01-22 13:48:01 +0100

    add post file on client

--- server/index.js
+++ server/index.js
@@ -17,6 +17,7 @@ app.use(bodyParser.json({limit: "30mb", extended: true}))
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}))
 app.use(cors());
 
+const CONNECTION_URL = 'mongodb+srv://javascriptmastery:faris5016@cluster0.yp6xf.mongodb.net/<dbname>?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
 //database connect
--- server/routes/post.js
+++ server/routes/post.js
@@ -1,9 +1,9 @@
 import express from 'express';
+import {getPosts, createPost} from '../controllers/post.js';
 
 const router = express.Router();
 
-router.get('/' , (req, res) => {
-    res.send('THIS WORKS')
-});
+router.get('/' , getPosts);
+router.post('/' , createPost);
 
 export default router;
