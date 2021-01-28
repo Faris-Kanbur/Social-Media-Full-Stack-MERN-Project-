862887d582a492679734eb7646adfb82d7378507
faris <faris38kanbur@gmail.com>
2021-01-28 15:16:30 +0100

    add update

--- server/controllers/post.js
+++ server/controllers/post.js
@@ -1,13 +1,29 @@
+import express from 'express';
+import mongoose from 'mongoose';
+
 import PostMessage from '../models/postMessage.js';
 
-export const getPosts = async (req, res) => {
+const router = express.Router();
+
+export const getPosts = async (req, res) => { 
     try {
         const postMessages = await PostMessage.find();
-
+                
         res.status(200).json(postMessages);
-
     } catch (error) {
-        res.status(404).json({ message: error.message});
+        res.status(404).json({ message: error.message });
+    }
+}
+
+export const getPost = async (req, res) => { 
+    const { id } = req.params;
+
+    try {
+        const post = await PostMessage.findById(id);
+        
+        res.status(200).json(post);
+    } catch (error) {
+        res.status(404).json({ message: error.message });
     }
 }
 
@@ -19,9 +35,20 @@ export const createPost = async (req, res) => {
     try {
         await newPost.save();
 
-        res.status(200).json(newPost);
+        res.status(201).json(newPost);
     } catch (error) {
-        res.status(409).json({ message: error.message})
-        
+        res.status(409).json({ message: error.message });
     }
+}
+
+
+export const updatePost = async (req, res) => {
+    const { id: _id } = req.params;
+    const post = req.body;
+
+    if (!mongoose.Types.ObjectId.isValid) return res.status(404).send('No Post with that id')
+
+    const updatedPost = await postMessage.findByIdAndUpdate(_id, post, {new: true});
+
+    res.json(updatedPost);
 }
