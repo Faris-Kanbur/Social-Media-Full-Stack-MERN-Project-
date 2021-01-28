2b0eb96c2fb5606dbd838eccb68be1baed0ace6d
faris <faris38kanbur@gmail.com>
2021-01-28 15:16:01 +0100

    add update func

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import { useDispatch} from 'react-redux';
 
+import { getPosts } from './actions/posts'
 import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
 import memories from './images/memories.png';
 import useStyles from './styles';
 
 const App = () => {
+    const [currentId, setCurrentId] = useState(null);
     const classes = useStyles();
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getPosts());
+    }, [dispatch]);
+
     return (
         	<Container maxidth="lg">
                 <AppBar className={classes.appBar} position="static" color="inherit">
@@ -23,7 +32,7 @@ const App = () => {
                                 <Posts />
                             </Grid>
                             <Grid item xs={12} sm={4}>
-                                <Form />
+                                <Form currentId={currentId} />
                             </Grid>
                         </Grid>
                     </Container>
