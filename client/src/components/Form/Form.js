2bb5368db17715cdd32cbb25ff58b0074f3bf564
faris <faris38kanbur@gmail.com>
2021-01-29 11:38:26 +0100

    add clear func

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -32,14 +32,17 @@ const Form = ({ currentId, setCurrentId}) => {
 
     };
 
-    const clear = () => {};
+    const clear = () => {
+        setCurrentId(null)
+        setPostData({ creator: '', title: '', message: '', tags: '', selectedFile: '' })
+    };
 
 
     return (
         
             <Paper className={classes.paper} >
                 <form autoComplete="off" noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit} >
-                    <Typography variant="h6">Let's share a moment</Typography>
+                    <Typography variant="h6"> {currentId ? "Editing" : "Let's share "} a moment</Typography>
                     <TextField name="creator" variant="outlined" label="Creator" fullWidth value={postData.creator} onChange={(e) => setPostData({ ...postData, creator: e.target.value })} />
                     <TextField name="title" variant="outlined" label="Title" fullWidth value={postData.title} onChange={(e) => setPostData({ ...postData, title: e.target.value })} />
                     <TextField name="message" variant="outlined" label="Message" fullWidth value={postData.message} onChange={(e) => setPostData({ ...postData, message: e.target.value })} />
