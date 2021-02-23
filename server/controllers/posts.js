b6b7bb802d23e2d7153b97c87e556768fd67a5ab
faris <faris38kanbur@gmail.com>
2021-02-23 13:37:33 +0100

    changed message still

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -31,12 +31,14 @@ export const createPost = async (req, res) => {
 
 
 export const updatePost = async (req, res) => {
-    const { id: _id } = req.params;
+    const {id} = req.params;
     const post = req.body;
+    
 
-    if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No Post with that id')
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No Post with that id')
 
-    const updatedPost = await PostMessage.findByIdAndUpdate(_id, {...post, _id}, {new: true});
+    
+    const updatedPost = await PostMessage.findByIdAndUpdate(id, {...post, id}, {new: true});
 
     res.json(updatedPost);
 }
@@ -54,12 +56,13 @@ export const deletePost = async (req, res) => {
 
 export const likePost = async (req, res) => {
     const {id} = req.params;
+    
 
     if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No post with that id')
 
     const post = await PostMessage.findById(id);
     const updatedPost = await PostMessage.findByIdAndUpdate(id, {likeCount: post.likeCount + 1}, {new:true})
     
-    res.json(updatePost);
+    res.json(updatedPost);
 
 }
